217e1cb62d8f51d450e8828f33f68e8f5d70da29
Chris Mac <cmac@rrc.ca>
2025-10-21 14:10:32 -0500

    add common word function

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ Assignment 3 – Text Stats
 from text_stats_utils import (
   clean_and_tokenize,
   get_unique_word_count,
-  get_longest_word
+  get_longest_word,
+  get_most_common_word
 )
 
-def get_most_common_word(words):
-    ...
 
 def get_letter_frequencies(text):
     ...
@@ -18,14 +17,15 @@ def main(input_text):
     words = clean_and_tokenize(input_text)
     unique_word_count = get_unique_word_count(words)
     longest_word = get_longest_word(words)
+    most_common_word = get_most_common_word(words)
 
     # Placeholder structure – we’ll fill these in.
     output_lines = [
       f"INPUT_TEXT: {input_text}",
       f"UNIQUE_WORDS: {unique_word_count}",
       f"LONGEST_WORD: {longest_word}",
-      f"MOST_COMMON_WORD: {None}",
-      f"MOST_COMMON_WORD_COUNT: {None}",
+      f"MOST_COMMON_WORD: {most_common_word["word"]}",
+      f"MOST_COMMON_WORD_COUNT: {most_common_word["count"]}",
       "LETTER_FREQUENCIES:", # a..z lines will go here
     ]
     
--- text_stats_utils.py
+++ text_stats_utils.py
@@ -23,3 +23,26 @@ def get_unique_word_count(words):
 
 def get_longest_word(words):
     return max(words, key=len)
+
+def get_most_common_word(words):
+    word_counts = {}
+    # loop over words
+    # append the word and the count to the word_counts dict
+    
+    for word in words:
+      if word not in word_counts:
+        word_counts[word] = 1
+      else:
+        word_counts[word] += 1
+    
+    most_common_word = None
+    highest_count = 0
+    
+    # Part 2
+    # loop through the word_counts
+    for word, count in word_counts.items():
+      if count > highest_count:
+        most_common_word = word
+        highest_count = count
+        
+    return {"word": most_common_word, "count": highest_count}
